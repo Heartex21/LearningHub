1429bd35b4e5652d0a46aa57c788b53b45dfd865
Heartex21 <burakcankaratas78@gmail.com>
2025-04-23 08:24:28 +0300

    BtL34 - Trolling Ethan Retry

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,7 +1,7 @@
 surface.CreateFont( "TheHeartexFont", {
 	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer.
 	extended = false,
-	size = 20,
+	size = 50,
 	weight = 500,
 	blursize = 0,
 	scanlines = 0,
@@ -18,8 +18,8 @@ surface.CreateFont( "TheHeartexFont", {
 
 hook.Add("HUDPaint", "DrawmyChoice", function()
 
-draw.RoundedBox(0,960,540,100,100,Color(120,255,120))
+draw.RoundedBox(0,800,540,200,100,Color(0,0,0))
 
-draw.SimpleText( "Ethan is a black man", "TheHeartexFont",960,540,Color(255,120,120),0,0)
+draw.SimpleText( "Ethan is a black man", "TheHeartexFont",800,540,Color(255,120,120),0,0)
 
 end)
