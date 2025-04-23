9a15701c2c8265e492dcee03acbf56489e42741d
Heartex21 <burakcankaratas78@gmail.com>
2025-04-23 08:18:52 +0300

    BtL32 - Hud Drawing and Text

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,6 +1,25 @@
+surface.CreateFont( "TheHeartexFont", {
+	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer.
+	extended = false,
+	size = 20,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
 hook.Add("HUDPaint", "DrawmyChoice", function()
 
-    draw.RoundedBox(0,0,0,100,100,Color(120,255,120))
-    
-    
-    end)
+draw.RoundedBox(0,0,0,100,100,Color(120,255,120))
+
+draw.SimpleText( "Hello World!", "TheHeartexFont",0,0,Color(255,120,120),0,0)
+
+end)
