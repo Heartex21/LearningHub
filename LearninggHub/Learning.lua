5b7f440e8773da6060288b73366bc3fc31493147
Heartex21 <burakcankaratas78@gmail.com>
2025-04-24 20:35:14 +0300

    BtL56 - Print message when button pressed

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -4,10 +4,15 @@ frame:Center()
 frame:SetVisible(true)
 frame:MakePopup()
 
+function buttonPressed()
+	print("Button was pressed!")
+end
+
 local button = vgui.Create("DButton", frame)
 button:SetPos(10,40)
 button:SetSize(200,45)
 button:SetText("Hello World")
+button.DoClick = buttonPressed
 
 local label = vgui.Create("DLabel", frame)
 label:SetPos(10,90)
