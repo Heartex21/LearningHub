75f4f1e09449e17ed68cdb3a74dab6a1086abcaf
Heartex21 <burakcankaratas78@gmail.com>
2025-04-24 20:21:16 +0300

    BtL51 - Derma Panel

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,27 +1,5 @@
-surface.CreateFont( "TheHeartexFont", {
-	font = "Arial", -- Use the font-name which is shown to you by your operating system Font Viewer.
-	extended = false,
-	size = 25,
-	weight = 500,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
-} )
-
-hook.Add("HUDPaint", "DrawmyChoice", function()
-
-local health = LocalPlayer():Health()
-
-draw.RoundedBox(0,8,ScrH() - 100,300+4, 30+4,Color(40,40,40))
-draw.RoundedBox(0,10,ScrH() - 98,health*3,30,Color(255,120,120))
-draw.SimpleText(health.."%", "TheHeartexFont",10+150,10+15,Color(255,255,255),1,1)
-
-end)
+local frame = vgui.Create("Frame")
+frame:SetSize(800, 600)
+frame:SetPos(12,12)
+frame:SetVisible(true)
+frame:MakePopup()
