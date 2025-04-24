f5be250e6bb4d3ffdb0d9b418a495eea6dac365e
Heartex21 <burakcankaratas78@gmail.com>
2025-04-24 21:13:26 +0300

    BtL64 - Trying something on the button

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -7,27 +7,19 @@ frame.Paint = function(s , w , h)
 
 draw.RoundedBox(5,0,0,w,h,Color(0,0,0))
 draw.RoundedBox(5,2,2,w-4,h-4,Color(50,50,50))
-end
-
-function buttonPressed()
-	print("Button was pressed!")
-end
 
 local button = vgui.Create("DButton", frame)
-button:SetPos(10,40)
-button:SetSize(200,45)
-button:SetText("Hello World")
-button.DoClick = buttonPressed
+button:SetSize(200, 50)
+button:Center()
+button:SetText("Click me to learn more!")
+button.DoClick = OnDepressed
 
-local label = vgui.Create("DLabel", frame)
-label:SetPos(10,90)
-label:SetText("This is a label")
-label:SetMouseInputEnabled(true)
-label.OnDepressed = function(s)
-	s:SetText("Label was clicked!")
-	label:SizeToContents()
+function OnDepressed()
+	button:SetText("You are clicking and holding me!")
+	button:SizeToContents()
 end
-label.OnReleased = function(s)
-	s:SetText("Label was released!")
-	label:SizeToContents()
-end
+
+button:OnReleased = function(s)
+	button:SetText(You have released me!")
+	button:SizeToContents()
+end")
