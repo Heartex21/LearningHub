87cb0aa62ed3ba8cf2429bfb291566a649d44479
Heartex21 <burakcankaratas78@gmail.com>
2025-04-26 08:03:43 +0300

    BtL86 - Networking #1

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,22 +1,13 @@
-local frame = vgui.Create("Frame")
-frame:SetSize(800, 600)
-frame:Center()
+local frame = vgui.Create("DFrame")
+frame:SetSize(100,100)
 frame:SetVisible(true)
+frame:Center()
 frame:MakePopup()
-frame.Paint = function(s , w , h)
 
-	draw.RoundedBox(5,0,0,w,h,Color(0,0,0))
-	draw.RoundedBox(5,2,2,w-4,h-4,Color(50,50,50))
+local heat = vgui.Create("DButton", frame)
+heat.DoClick = function()
 
-end
+net.Start("LearningHub4")
+net.SendToServer()
 
-local DComboBox = vgui.Create("DComboBox", frame)
-DComboBox:SetPos(300,250)
-DComboBox:SetSize(200,50)
-DComboBox:SetValue("Select an option")
-DComboBox:AddChoice("I Like learning.")
-DComboBox:AddChoice("I like seeing new things.")
-DComboBox:AddChoice("Thats pretty much all today.")
-DComboBox.OnSelect = function(self, index, value)
-	print(value.."was selected at index"..index)
 end
