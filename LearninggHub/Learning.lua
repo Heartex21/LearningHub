67928aba6c57ced4aacd93d9813a278bb86f0da4
Heartex21 <burakcankaratas78@gmail.com>
2025-04-29 21:27:53 +0300

    BtL108 - Fix

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,6 +1,6 @@
 local test = Lerp(LocalPlayer():Health()/100, 0, 1000)
 print(test)
 
-hook.Add("HudPaint", "heartex", function()
+hook.Add("HUDPaint", "heartex", function()
 	draw.RoundedBox(0,0,0,1000,100,Color(255,120,120,255))
 end)
