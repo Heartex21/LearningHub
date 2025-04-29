c2fb80a39e6dd44a3e2bee4d70d0be5a2ffbdf54
Heartex21 <burakcankaratas78@gmail.com>
2025-04-29 21:18:57 +0300

    BtL103 - Learning Lerp

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,20 +1,2 @@
-local frame = vgui.Create("DFrame")
-frame:SetSize(100,100)
-frame:SetVisible(true)
-frame:Center()
-frame:MakePopup()
-
-local heat = vgui.Create("DButton", frame)
-heat.DoClick = function()
-
-net.Start("LearningHub4")
-net.WriteInt(32, 16)
-net.SendToServer()
-
-end
-
-net.Receive("ClientMessage", function(len)
-
-	print("I received the message from the server")
-
-end)
+local test = Lerp(0, 0, 100)
+print(test)
