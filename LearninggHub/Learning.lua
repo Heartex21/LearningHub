43593d37fb5ff6c568af9e3bbadc38cbd216a602
Heartex21 <burakcankaratas78@gmail.com>
2025-04-29 21:32:22 +0300

    BtL110 - Slower healthbar test

--- LearninggHub/Learning.lua
+++ LearninggHub/Learning.lua
@@ -1,6 +1,6 @@
 local smoothHealth = 100
 
 hook.Add("HUDPaint", "heartex", function()
-	smoothHealth = Lerp(10*FrameTime(), smoothHealth, LocalPlayer():Health())
+	smoothHealth = Lerp(2*FrameTime(), smoothHealth, LocalPlayer():Health())
 	draw.RoundedBox(0,0,0,smoothHealth*5,100,Color(255,120,120,255))
 end)
